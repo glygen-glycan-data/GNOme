41638b21c7a98b2e4d328ab2f12df000cddc1c90
Nathan Edwards <edwardsnj@gmail.com>
2019-05-31 13:19:45 -0400

    Tweak convert.sh script, permissions and user id

--- convert.sh
+++ convert.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-docker run -u 501:501 -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input GNOme.owl --format obo --output GNOme.obo
-docker run -u 501:501 -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input GNOme.owl --format json --output GNOme.json
+USER="`id -u`:`id -g`"
+docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input GNOme.owl --format obo --output GNOme.obo
+docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input GNOme.owl --format json --output GNOme.json
