dd941a3e8c3208b8432ad8666c77a2f77aba5549
Nathan Edwards <edwardsnj@gmail.com>
2019-06-03 11:37:34 -0400

    restricted ontologies for GlyGen

--- convert.sh
+++ convert.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+set -x
 USER="`id -u`:`id -g`"
-docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input GNOme.owl --format obo --output GNOme.obo
-docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input GNOme.owl --format json --output GNOme.json
+for OWL in `find . -name "*.owl"`; do
+  DIR=`dirname "$OWL"`
+  BASE=`basename "$OWL" .owl`
+  OBO="$DIR/$BASE.obo"
+  JSON="$DIR/$BASE.json"
+  if [ \( \! -f "$OBO" \) -o \( "$OWL" -nt "$OBO" \) ]; then
+    docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input "$OWL" --format obo --output "$OBO"
+  fi
+  if [ \( \! -f "$JSON" \) -o \( "$OWL" -nt "$JSON" \) ]; then
+    docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input "$OWL" --format json --output "$JSON"
+  fi
+done
