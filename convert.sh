ee62c4f77ea44c9ef1369daf03888b254c7e15d2
Nathan Edwards <edwardsnj@gmail.com>
2022-12-14 14:10:06 -0500

    update convert.sh for new robot container

--- convert.sh
+++ convert.sh
@@ -7,9 +7,9 @@ for OWL in `find . -name "*.owl"`; do
   OBO="$DIR/$BASE.obo"
   JSON="$DIR/$BASE.json"
   if [ \( \! -f "$OBO" \) -o \( "$OWL" -nt "$OBO" \) ]; then
-    docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input "$OWL" --format obo --output "$OBO"
+    docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm obolibrary/robot robot convert --input "$OWL" --format obo --output "$OBO"
   fi
   if [ \( \! -f "$JSON" \) -o \( "$OWL" -nt "$JSON" \) ]; then
-    docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm robot convert --input "$OWL" --format json --output "$JSON"
+    docker run -u $USER -v `pwd`:/tmp/working -w /tmp/working --rm obolibrary/robot robot convert --input "$OWL" --format json --output "$JSON"
   fi
 done
